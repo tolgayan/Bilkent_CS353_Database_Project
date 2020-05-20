432ff0305f39bcab6867067a6490c97e700f171f
Enes <ensmrdane@gmail.com>
2020-05-20 20:03:47 +0300

    mainPage.ejs & some routers
    - mainPage.ejs is created.
    - createTaskPageRouter.js, mainPageRouter.js, previousTasksPageClubsRouter.js are created.

---
+++ routes/previousTasksPageClubsRouter.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const router = express.Router();
+const db = require('../public/javascripts/db');
+
+let session;
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.sendFile( 'previousTasksPageClubs.html', {root: 'public/html'});
+});
+
+router.post('/', function (req, resp){
+  //resp.end(JSON.stringify(response.body));
+  // session = req.session;
+  // let sql = `SELECT id FROM user WHERE username = '${req.body.username}' AND password = '${req.body.password}'`;
+  // db.query(sql, (err, result) => {
+  //     if(err)
+  //         throw err;
+  //     if(result.length > 0){
+  //         //resp.send("Logged");
+  //         resp.redirect('/');
+  //         session.uniqueID = result[0].id;
+  //     }
+  //     else
+  //         resp.end("Wrong username or password!");
+  // });
+});
+
+module.exports = router;
