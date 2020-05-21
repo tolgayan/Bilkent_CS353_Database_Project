7718823dbe4c584d0c655385b34a1e096985cfaf
Enes <ensmrdane@gmail.com>
2020-05-21 23:48:09 +0300

    Bir çeşit denemeler
    Bir çeşit denemeler

--- routes/addpost.js
+++ routes/addpost.js
@@ -6,48 +6,47 @@ const db = require('../public/javascripts/db');
 
 
 router.get("/", function (req, res, next) {
-    if (!req.session.user) {
-      res.redirect("../");
-    } else {
-      let sql = "SELECT * FROM user WHERE user_id = " + req.session.user;
-  
-      db.query(sql, function (err, data, fields) {
-        if (err) throw err;
-  
-        let usertype = data[0].usertype;
-  
-        if (usertype == "editor") res.render("add_post");
-        else res.redirect("../");
-      });
-    }
-  });
+  if (!req.session.user) {
+    res.redirect("../");
+  } else {
+    let sql = "SELECT * FROM user WHERE user_id = " + req.session.user;
 
-  router.post("/", function (req, res) {
-    if(req.session.user) {
-      let sql = "SELECT * FROM user WHERE user_id = " + req.session.user;
-  
-      db.query(sql, function (err, data, fields) {
-        if (err) throw err;
-  
-        let usertype = data[0].usertype;
-  
-        if (usertype == "editor") {
-          let values = {
-            title: req.body.title,
-            category: req.body.category,
-            content: req.body.content,
-            editor_id: data[0].user_id,     
-          }
+    db.query(sql, function (err, data, fields) {
+      if (err) throw err;
 
-          let stmt = "INSERT INTO news SET ?";
-          db.query(stmt, values, (err, result) => {
-            if (err) throw err;
-            res.redirect("../news");
-          });
+      let usertype = data[0].usertype;
 
-        };
-      });
-    }
-  });
-  
-  module.exports = router;
+      if (usertype == "editor") res.render("add_post");
+      else res.redirect("../");
+    });
+  }
+});
+
+router.post("/", function (req, res) {
+  if (req.session.user) {
+    let sql = "SELECT * FROM user WHERE user_id = " + req.session.user;
+
+    db.query(sql, function (err, data, fields) {
+      if (err) throw err;
+
+      let usertype = data[0].usertype;
+
+      if (usertype == "editor") {
+        let values = {
+          title: req.body.title,
+          category: req.body.category,
+          content: req.body.content,
+          editor_id: data[0].user_id,
+        }
+
+        let stmt = "INSERT INTO news SET ?";
+        db.query(stmt, values, (err, result) => {
+          if (err) throw err;
+          res.redirect("../news");
+        });
+      };
+    });
+  }
+});
+
+module.exports = router;
--- routes/createTaskPage.js
+++ routes/createTaskPage.js
@@ -20,21 +20,73 @@ router.get('/', function (req, res, next) {
 
   });
 
-  router.post('/', function (req, resp) {
-    // resp.end(JSON.stringify(response.body));
-    // session = req.session;
-    // let sql = `SELECT id FROM user WHERE username = '${req.body.username}' AND password = '${req.body.password}'`;
-    // db.query(sql, (err, result) => {
-    //     if(err)
-    //         throw err;
-    //     if(result.length > 0){
-    //         //resp.send("Logged");
-    //         resp.redirect('/');
-    //         session.uniqueID = result[0].id;
-    //     }
-    //     else
-    //         resp.end("Wrong username or password!");
-    // });
+  // router.get('/' + agency_id, function (req, res, next) {
+  //   let sql = "INSERT INTO task (gender, height_min, height_max, weight_min, weight_max, age_min, age_max, position, time, \
+  //     scout_num, preferred_foot, salary_min, salary_max, transfer_price_max, transfer_price_min)";
+
+  //   if (err) throw err;
+
+  //   let usertype = data[0].usertype;
+
+  //   if (usertype == "scout_agency") {
+  //     let values = {
+  //       gender: req.body.gender,
+  //       height_min: req.body.height_min,
+  //       height_max: req.body.height_max,
+  //       weight_min: req.body.weight_min,
+  //       weight_max: req.body.weight_max,
+  //       age_min: req.body.age_min,
+  //       age_max: req.body.age_max,
+  //       position: req.body.position,
+  //       time: req.body.time,
+  //       scout_num: req.body.number_of_scouts,
+  //       preferred_foot: req.body.preferred_foot,
+  //       salary_min: req.body.salary_min,
+  //       salary_max: req.body.salary_max,
+  //       transfer_price_max: req.body.transfer_price_max,
+  //       transfer_price_min: req.body.transfer_price_min,
+  //       agency_id: agency_id
+  //     }
+  //     console.log(values);
+  //   }
+  // });
+
+  router.post("/", function (req, resp) {
+    let sql = "INSERT INTO task (foot, position, time, scout_num, assigned_date, status, agency_id, transfer_price_min, transfer_price_max, salary_min, salary_max, age_min, age_max, weight_min, weight_max, height_min, height_max) VALUES ";
+    console.log("why dont you work ")
+    // if (err) throw err;
+    // let usertype = data[0].usertype;
+    // console.log(usertype);
+    // if (usertype == "scout_agency") {
+    console.log(req.body.gender)
+    let values = "( \
+      req.body.preferred_foot, \
+      req.body.position, \
+      req.body.time,  \
+      req.body.number_of_scouts," +
+      new Date().toString() +
+      ", \'complated\', \
+      req.body.agency_id, \
+      req.body.transfer_price_min, \
+      req.body.transfer_price_max, \
+      req.body.age_min, \
+      req.body.age_max, \
+      req.body.salary_min, \
+      req.body.salary_max, \
+      req.body.weight_min, \
+      req.body.weight_max, \
+      req.body.height_min, \
+      req.body.height_max, \
+      )"
+
+    sql = sql + values;
+    console.log(sql);
+    db.query(sql, function (err, data, fields) {
+      if (err) throw err;
+
+      //res.redirect("../");
+    });
+    // }
   });
 });
 module.exports = router;
